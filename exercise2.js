d53a50355c89e6e0c2fae12b02073efcfb8b468f
andtan <jornandretangen@gmail.com>
2012-10-22 23:52:39 +0200

    excercise 2 work

--- exercise2.js
+++ exercise2.js
@@ -10,6 +10,33 @@ var List = require("./lib/List"),
     concat = List.concat
 
 
+function caseof( caase, patterns ) {
+  var i, pattern, fn
+  for( i = 0; i < patterns.length; i++ ) {
+    pattern = patterns[i][0]
+    fn = patterns[i][1]
+    params = matchesRecord(caase, pattern)
+    if( params ) {
+      return fn.apply( fn, params )
+    }
+    
+  }
+}
+
+function matchesRecord( obj, pattern ) {
+  var pair, id, expr, val
+  pair = pattern.split(":")
+  id = pair[0]
+  expr = pair[1]
+  if( id in obj ) {
+    val = obj[ id ]
+    if( Function("N", "return " + expr).call({}, val) ) {
+      return [ val ]
+    }
+  }
+  return null
+}
+
 function applyMoves( state, moves ) {
   var move, newState
   if( len(moves) === 0 ) {
@@ -20,10 +47,50 @@ function applyMoves( state, moves ) {
     return List()
   }
   return caseof(move, [
-    "trackA: (N > 0)": function() {},
-    "trackA: (N < 0)": function() {},
-    "trackB: (N > 0)": function() {},
-    "trackB: (N < 0)": function() {}
+    ["trackA: N > 0", function( n ) {
+      newState = {
+        trackB: state.trackB,
+        trackA: append( drop(state.main, len(state.main) - n), state.trackA ),
+        main: take( state.main, len(state.main) - n )
+      }
+      return concat( List([ newState ]), applyMoves(newState, moves.tail()) )
+    }],
+    ["trackA: N < 0", function( n ) {
+      newState = {
+        trackB: state.trackB,
+        trackA: drop( state.trackA, (n * -1) ),
+        main: append( state.main, take( state.trackA, (n * -1) ) )
+      }
+      return concat( List([ newState ]), applyMoves(newState, moves.tail()) )
+    }],
+    ["trackB: N > 0", function( n ) {
+      newState = {
+        trackB: append( drop(state.main, len(state.main) - n), state.trackB ),
+        trackA: state.trackA,
+        main: take( state.main, len(state.main) - n )
+      }
+      return concat( List([ newState ]), applyMoves(newState, moves.tail()) )
+    }],
+    ["trackB: N < 0", function( n ) {
+      newState = {
+        trackB: drop( state.trackB, (n * -1) ),
+        trackA: state.trackA,
+        main: append( state.main, take( state.trackB, (n * -1) ) )
+      }
+      return concat( List([ newState ]), applyMoves(newState, moves.tail()) )
+    }]
   ])
-  return concat( List([ newState ]), applyMoves(newState, moves.tail()) )
 }
+
+console.log(JSON.stringify(
+  applyMoves({
+    trackA: List([]),
+    trackB: List([]),
+    main: List(["a", "b"])
+  },List([
+    {trackA: 1},
+    {trackB: 1},
+    {trackA: -1},
+    {trackB: -1}
+  ]))
+))
